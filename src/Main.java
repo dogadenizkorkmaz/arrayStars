5659429df5b8fab977408ff6664572074546b8bb
Doga <dogadenizkorkmaz@gmail.com>
2024-03-01 08:47:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        String[][] letter = new String[6][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 2 ) {
+                    letter[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
+                }if(i == 5){ // verilen örneğe ek olarak sadece ilave bir if ekledim
+                    letter[i][j] = " * ";
+                }
+            }
+        }
+
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
